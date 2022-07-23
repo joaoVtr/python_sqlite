0a57a21f8320b8230b5921f5b06ddd37becc540d
João Vitor Menezes de Jesus <joao_vitor_mj@hotmail.com>
2022-07-23 00:25:55 -0300

    feat/select data

--- select_database.py
+++ select_database.py
@@ -7,9 +7,14 @@ c = conn.cursor()
 c.execute('SELECT * FROM costumers')
 
 # print(c.fetchone())
+# print(c.fetchone()[1])
 # print(c.fetchmany(3))
-print(c.fetchall())
+# print(c.fetchmany(3)[2])
 
+items = c.fetchall()
+
+for item in items:
+    print(item[0] + '\t' + item[1] +'\t' + item[2])
 
 conn.commit()
 
